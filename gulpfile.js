dadab4bddaba267f3b5a5c7a665417a20b637b3c
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-01-04 10:09:06 -0500

    time to get to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,165 @@
-'use strict';
+// generated on 2018-01-04 using generator-webapp 3.0.1
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const wiredep = require('wiredep').stream;
+const runSequence = require('run-sequence');
 
-var browserSync = require('browser-sync').create(),
-    path = require('path'),
-    del = require('del'),
-    gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    notify = require('gulp-notify'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    less = require('gulp-less'),
-    runSequence = require('run-sequence');
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 
-// Task to compile SCSS
-gulp.task('sass', function () {
-  return gulp.src('./scss/styles.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'nested',
-      errLogToConsole: false,
-      paths: [path.join(__dirname, 'scss', 'includes')]
-    })
-      .on("error", notify.onError(function (error) {
-        return "Failed to Compile SCSS: " + error.message;
-      })))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./css/'))
-    .pipe(browserSync.stream())
-    .pipe(notify({
-      message: "SCSS Compiled Successfully :)",
-      onLast: true
-    }));
+let dev = true;
+
+gulp.task('styles', () => {
+  return gulp.src('app/styles/*.css')
+    .pipe($.if(dev, $.sourcemaps.init()))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.if(dev, $.sourcemaps.write()))
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
 });
 
-// Task to compile LESS
-gulp.task('less', function () {
-  return gulp.src('./less/styles.less')
-    .pipe(sourcemaps.init())
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    })
-    .on("error", notify.onError(function(error) {
-      return "Failed to Compile LESS: " + error.message;
+gulp.task('scripts', () => {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.plumber())
+    .pipe($.if(dev, $.sourcemaps.init()))
+    .pipe($.babel())
+    .pipe($.if(dev, $.sourcemaps.write('.')))
+    .pipe(gulp.dest('.tmp/scripts'))
+    .pipe(reload({stream: true}));
+});
+
+function lint(files) {
+  return gulp.src(files)
+    .pipe($.eslint({ fix: true }))
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.eslint.format())
+    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+}
+
+gulp.task('lint', () => {
+  return lint('app/scripts/**/*.js')
+    .pipe(gulp.dest('app/scripts'));
+});
+gulp.task('lint:test', () => {
+  return lint('test/spec/**/*.js')
+    .pipe(gulp.dest('test/spec'));
+});
+
+gulp.task('html', ['styles', 'scripts'], () => {
+  return gulp.src('app/*.html')
+    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
+    .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
+    .pipe($.if(/\.html$/, $.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: {compress: {drop_console: true}},
+      processConditionalComments: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
     })))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./css/'))
-    .pipe(browserSync.stream())
-    .pipe(notify({
-      message: "LESS Compiled Successfully :)",
-      onLast: true
-    }));
+    .pipe(gulp.dest('dist'));
 });
 
-// BrowserSync Task (Live reload)
-gulp.task('browserSync', function () {
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin()))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+    .concat('app/fonts/**/*'))
+    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', () => {
+  runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
+    browserSync.init({
+      notify: false,
+      port: 9000,
+      server: {
+        baseDir: ['.tmp', 'app'],
+        routes: {
+          '/bower_components': 'bower_components'
+        }
+      }
+    });
+
+    gulp.watch([
+      'app/*.html',
+      'app/images/**/*',
+      '.tmp/fonts/**/*'
+    ]).on('change', reload);
+
+    gulp.watch('app/styles/**/*.css', ['styles']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/fonts/**/*', ['fonts']);
+    gulp.watch('bower.json', ['wiredep', 'fonts']);
+  });
+});
+
+gulp.task('serve:dist', ['default'], () => {
   browserSync.init({
+    notify: false,
+    port: 9000,
     server: {
-      baseDir: "./"
+      baseDir: ['dist']
     }
   });
 });
 
-// Gulp Watch Task
-gulp.task('watch', ['browserSync'], function () {
-  gulp.watch('./scss/**/*', ['sass']);
-  gulp.watch('./less/**/*', ['less']);
-  gulp.watch('./**/*.html').on('change', browserSync.reload);
+gulp.task('serve:test', ['scripts'], () => {
+  browserSync.init({
+    notify: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test',
+      routes: {
+        '/scripts': '.tmp/scripts',
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch(['test/spec/**/*.js', 'test/index.html']).on('change', reload);
+  gulp.watch('test/spec/**/*.js', ['lint:test']);
 });
 
-// Gulp Clean Up Task
-gulp.task('clean', function () {
-  return del('./css/');
+// inject bower components
+gulp.task('wiredep', () => {
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      exclude: ['bootstrap.js'],
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
 });
 
-// Gulp Default Task
-gulp.task('default', ['watch']);
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
 
-// Gulp Build Task
-gulp.task('build', function (callback) {
-  runSequence('clean', 'sass', callback);
+gulp.task('default', () => {
+  return new Promise(resolve => {
+    dev = false;
+    runSequence(['clean', 'wiredep'], 'build', resolve);
+  });
 });
