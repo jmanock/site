cd7cffdbebe8d8df60ee8392d1f4e08a8419b6e4
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-01-06 17:08:53 -0500

    navbar working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,10 @@
-function myFunction(x){
+function hBar(x){
   x.classList.toggle('change');
+  // var value = $('#navigation').css('right') === '100px' ? 0 : '100px';
+  // $('#navigation').toggle('show');
+  // $('#navigation').animate({
+  //   right:value
+  // });
+  $('#navigation').toggle('show');
+  
 }
