d49fdb57bced0b0ab560209c712bdf7ccbe46012
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-01-25 12:00:50 -0500

    finally got it to work, now to add menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,6 +10,10 @@ $(document).ready(function(){
     }
   });
   $('.menu-btn').on('click', function(){
-    
+    if($('.menu-btn').hasClass('active')){
+      $('.menu-btn').removeClass('active');
+    }else{
+      $('.menu-btn').addClass('active');
+    }
   });
 });
