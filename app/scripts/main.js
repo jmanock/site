5498410e72a679e22cc93026dc395413e52d3707
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-01-25 13:05:53 -0500

    first section done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
-  var scrollTop = 0;
-  $(window).scroll(function(){
-    scrollTop = $(window).scrollTop();
-    $('.counter').html(scrollTop);
-    if(scrollTop >= 400){
-      $('#global-nav').addClass('scrolled-nav');
-    }else if(scrollTop < 400){
-      $('#global-nav').removeClass('scrolled-nav');
-    }
-  });
+  
   $('.menu-btn').on('click', function(){
     if($('.menu-btn').hasClass('active')){
       $('.menu-btn').removeClass('active');
