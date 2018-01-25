9186e87286f6298fb14fbd6817d127029c8156ec
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-01-25 18:04:36 -0500

    title works on scroll, found a bug

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  
+  var header = $('.site-title');
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll >= 300){
+      header.addClass('scrolled');
+    }else{
+      header.removeClass('scrolled');
+    }
+  });
   $('.menu-btn').on('click', function(){
     if($('.menu-btn').hasClass('active')){
       $('.menu-btn').removeClass('active');
