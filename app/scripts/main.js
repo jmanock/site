d322693367820ee47a29e9bb8ab85db151d30cb7
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-01-30 09:21:55 -0500

    better idea for the works section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   var header = $('.site-title');
+
+  // Changes font size and color on scroll
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if(scroll >= 300){
@@ -8,11 +10,25 @@ $(document).ready(function(){
       header.removeClass('scrolled');
     }
   });
+
+  // Changes hamburger menu button on click
   $('.menu-btn').on('click', function(){
     if($('.menu-btn').hasClass('active')){
       $('.menu-btn').removeClass('active');
     }else{
       $('.menu-btn').addClass('active');
+    }
+  });
+
+  // Need to add buttons for the portfolio
+  // Idea would be Apps Website and All
+  $('.pButton').on('click', function(){
+    if($(this).val === ('Website')){
+      // Show Websites only
+    }else if($(this).val === ('Apps')){
+      // Show Apps only
+    }else{
+      // Show All
     }
   });
 });
