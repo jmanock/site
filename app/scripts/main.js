19564597fb9fecc130d9f937df4726bd8ea65cdc
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-02-03 00:00:27 -0500

    restyle with grids

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var header = $('.site-title');
 
-  // Changes font size and color on scroll
+  /* Changes font-size & color on scroll */
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if(scroll >= 300){
@@ -9,26 +9,15 @@ $(document).ready(function(){
     }else{
       header.removeClass('scrolled');
     }
-  });
+  });/* End `Scroll` */
 
-  // Changes hamburger menu button on click
+  /* Changes `Hamburger Menu` */
   $('.menu-btn').on('click', function(){
     if($('.menu-btn').hasClass('active')){
       $('.menu-btn').removeClass('active');
     }else{
       $('.menu-btn').addClass('active');
     }
-  });
+  });/* End `Click` */
 
-  // Need to add buttons for the portfolio
-  // Idea would be Apps Website and All
-  $('.pButton').on('click', function(){
-    if($(this).val === ('Website')){
-      // Show Websites only
-    }else if($(this).val === ('Apps')){
-      // Show Apps only
-    }else{
-      // Show All
-    }
-  });
-});
+});/* End `Ready` */
