9f9634cd261b1ab7da8a6f6d90e0717a96028409
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2018-02-03 03:45:05 -0500

    finally got works to work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,5 +19,13 @@ $(document).ready(function(){
       $('.menu-btn').addClass('active');
     }
   });/* End `Click` */
-
+  $('.grid').isotope({
+    itemSelector:'.grid-item',
+  });
+  $('.filter-button-group').on('click','li', function(){
+    var filterValue = $(this).attr('data-filter');
+    $('.grid').isotope({filter:filterValue});
+    $('.filter-button-group li').removeClass('active');
+    $(this).addClass('active');
+  });
 });/* End `Ready` */
